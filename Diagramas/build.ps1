a220fe6c2e365f5d20789071e0fa48d085d413ed
gael <gaellgonzalez10@gmail.com>
2024-10-07 19:58:06 -0300

    Constructores agregados, detalles de clases, constructor de pdfs

---
+++ Diagramas/build.ps1
@@ -0,0 +1,7 @@
+$plantuml="C:\Users\gaelr\OneDrive\Escritorio\Nomolestar\plantuml.jar"
+$defensa = 4
+mkdir -Force -p build/images
+java -jar $plantuml -tsvg diagramas/diagrama-*.puml -o ../build/images
+pandoc -fmarkdown-implicit_figures -o "Diagrama-extendido-defensa-$defensa.pdf" build/diagrama-extendido.md
+pandoc -fmarkdown-implicit_figures -o "Diagrama-reducido-defensa-$defensa.pdf" build/diagrama-reducido.md
+
