6279a9107e383d4e910bdc5c3f87271f84b5b4b3
FrancoDM926 <francodemattia926@gmail.com>
2024-10-22 22:20:42 -0300

    fisicas horizontales incompleto

--- src/entities/Mario.java
+++ src/entities/Mario.java
@@ -2,8 +2,11 @@ package entities;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 
+import javax.swing.ImageIcon;
+
 import colliders.Direction;
 import colliders.MarioCollider;
+import game.CollisionsEngine;
 import game.Game;
 import game.KeyStatus;
 import graphics.GameGraphicElement;
@@ -12,18 +15,26 @@ public class Mario extends BaseUpdatableEntity {
     protected static String SPRITES_FOLDER = "mario";
     protected int speedX;
     protected int speedY;
+    protected int accelerationX;
+    protected int decelerationX;
+    protected int maxSpeed = 8;
     protected int lifes;
     protected boolean loaded;
     protected boolean jumping;
     protected Direction direction;
     protected float gravity;
     protected int jumpForce;
+    protected boolean dead;
+    protected boolean moving;
 
     public Mario() {
         speedX = 4;
         speedY = -3;
         gravity = 2;
-        jumpForce = 10;
+        jumpForce = 0;
+        accelerationX = 1;
+        decelerationX = 2;
+        dead = false;
         loaded = false;
         jumping = false;
         collider = new MarioCollider(this, new Rectangle());
@@ -51,9 +62,12 @@ public class Mario extends BaseUpdatableEntity {
         if (Game.instance().getKeyStatus(KeyEvent.VK_W) == KeyStatus.PRESSED && !jumping) {
             startJump();
         }
-
+        if (!dead) {
         handleHorizontalMovement();
         handleVerticalMovement();
+        } else {
+            die();
+        }
     }
 
     public void land() {
@@ -63,7 +77,7 @@ public class Mario extends BaseUpdatableEntity {
 
     protected void startJump() {
         jumping = true;
-        jumpForce = 10;
+        speedY = 24;
         //graphicElement.setSprite(new ImageIcon("sprites/marioJumping.png"));
     }
 
@@ -71,33 +85,80 @@ public class Mario extends BaseUpdatableEntity {
         if (speedY > -5) {
             speedY -= gravity; 
         }
-
+        
         if (speedY > 20) {
             jumpForce = 0;
         }
-
-        speedY += jumpForce;
+        
+        //speedY += jumpForce;
 
         collider.translate(0, (int) speedY);
         graphicElement.translate(0, (int) speedY);
     }
 
     protected void handleHorizontalMovement() {
-        if (Game.instance().getKeyStatus(KeyEvent.VK_D) == KeyStatus.PRESSED) {
-            if(jumping) {
-                //graphicElement.setSprite(new ImageIcon("sprites/marioJumping.png"));
-            }
-            graphicElement.translate(speedX, 0);
-            collider.translate(speedX, 0);
-        }
+        moving = false;
+    
 
-        if (Game.instance().getKeyStatus(KeyEvent.VK_A) == KeyStatus.PRESSED) {
-            if(jumping) {
+        if (Game.instance().getKeyStatus(KeyEvent.VK_D) == KeyStatus.PRESSED) {
+            moving = true;
+    
+            if (jumping) {
+                //graphicElement.setSprite(new ImageIcon("sprites/marioRunning1.png"));
+            } else {
                 //graphicElement.setSprite(new ImageIcon("sprites/marioJumping.png"));
             }
-            graphicElement.translate(-speedX, 0);
-            collider.translate(-speedX, 0);
+    
+
+            if (speedX < maxSpeed) {
+                speedX += accelerationX;
+            }
         }
+    
+  
+        if (Game.instance().getKeyStatus(KeyEvent.VK_A) == KeyStatus.PRESSED) {
+            moving = true;
+    
+            if (jumping) {
+               // graphicElement.setSprite(new ImageIcon("sprites/marioRunning1.png"));
+            } else {
+                //graphicElement.setSprite(new ImageIcon("sprites/marioJumping.png"));
+            }
+    
+
+            if (speedX > -maxSpeed) {
+                speedX -= accelerationX;
+            }
+        }
+    
+        
+        if (!moving) {
+            if (speedX > 0) {
+                speedX -= decelerationX;
+                if (speedX < 0) speedX = 0;
+            } else if (speedX < 0) {
+                speedX += decelerationX;
+                if (speedX > 0) speedX = 0;
+            }
+        }
+    
+
+        graphicElement.translate(speedX, 0);
+        collider.translate(speedX, 0);
+    }
+
+    public void die() {
+        speedY -= gravity;
+        //graphicElement.setSprite(new ImageIcon("sprites/marioDeath.png"));
+        graphicElement.translate(0, speedY);
+    }
+
+    public void takeDamage() {
+        //aca va la logica que depende del estado
+        die();
+        CollisionsEngine.instance().remove(collider);
+        dead = true;
+        speedY = 15;
     }
 
 }
